3a0f2e9767cdc8e90f3b5d05e59ae4cf1cb7984a
Lassek7 <lasse.a.kristensen@outlook.com>
2023-04-03 21:44:28 +0200

    new changes

--- src/styles.tsx
+++ src/styles.tsx
@@ -3,11 +3,30 @@ const menuButtonsCTA = {
     borderRadius: '45px',
     paddingX: '16px',
     paddingY: '8px',
-    height: '70px'
+    height: '54px',
+    width: '153px',
+};
+
+const fullMapStyle = {
+  position: 'absolute',
+  borderRadius: '45px',
+  overflow: 'hidden',
+  left: '20px',
+  right: '20px',
+  top: '20px',
+  bottom: '110px'
+}
+
+const EmergencyStop = {
+  paddingX: '16px',
+  paddingY: '8px',
+  height: '70px'
 };
 
 const Styles = {
-  customButtonStyles: menuButtonsCTA
+  menuCTA: menuButtonsCTA,
+  emergency: EmergencyStop,
+  fullMap: fullMapStyle
 }
 
   export default Styles
